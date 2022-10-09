b9a6983b5978ae744519ee146566c352180a9247
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-10-09 07:32:39 +0300

    commit tree 57f25e8c3b2742477d1a2b2562ccef0702fd547c

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
-
+  
   def show
     article = Article.find(params[:id])
     render json: article
